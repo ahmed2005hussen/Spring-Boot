c9019dda9f71e66e7ae955a3536502fd8e67bd6d
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2026-01-28 21:23:38 +0200

    spring security 2

--- 04- spring boot rest security/02-spring-boot-rest-security-using-DB -Customize/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 04- spring boot rest security/02-spring-boot-rest-security-using-DB -Customize/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,36 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE").build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
+        // query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id , pw, active from members where user_id = ?");
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER").build();
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id , role from roles where user_id =?"
+        );
 
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return jdbcUserDetailsManager;
     }
 
     @Bean
@@ -47,7 +40,7 @@ public class DemoSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.PATCH,"/api/employees/**").hasRole("MANAGER"));
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER"));
         http.httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
 
