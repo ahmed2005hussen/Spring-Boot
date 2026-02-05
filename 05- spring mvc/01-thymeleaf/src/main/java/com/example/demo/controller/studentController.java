0502d7c3e2d73fae8a851b8f04eb518720d55803
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2026-02-05 17:02:01 +0200

    add some thymeleaf code

--- 05- spring mvc/01-thymeleaf/src/main/java/com/example/demo/controller/studentController.java
+++ 05- spring mvc/01-thymeleaf/src/main/java/com/example/demo/controller/studentController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class studentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${Languages}")
+    private List<String> languages;
+
+    @Value("${systems}")
+    private List<String> systems;
 
     @GetMapping("/studentShowForm")
     public String showForm(Model model){
@@ -16,13 +28,19 @@ public class studentController {
         Student student = new Student();
         model.addAttribute("student",student);
 
+        model.addAttribute("countries",countries);
+
+        model.addAttribute("languages",languages);
+
+        model.addAttribute("systems",systems);
+
         return "student-form";
     }
 
     @PostMapping("/processStudentForm")
     public String processForm(@ModelAttribute("student")  Student student ){
 
-        System.out.println(student.getFirstName() + " , " + student.getLastName());
+        System.out.println(student.getFirstName() + " , " + student.getLastName() + " " + student.getCountry() + " " + student.getFavoriteLanguage());
         return "student-confirmation";
     }
 
--- 05- spring mvc/01-thymeleaf/src/main/java/com/example/demo/model/Student.java
+++ 05- spring mvc/01-thymeleaf/src/main/java/com/example/demo/model/Student.java
@@ -1,11 +1,39 @@
 package com.example.demo.model;
 
 
+import java.util.List;
 
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteSystem;
+
+    public List<String> getFavoriteSystem() {
+        return favoriteSystem;
+    }
+
+    public void setFavoriteSystem(List<String> favoriteSystem) {
+        this.favoriteSystem = favoriteSystem;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 
     public Student() {}
 
