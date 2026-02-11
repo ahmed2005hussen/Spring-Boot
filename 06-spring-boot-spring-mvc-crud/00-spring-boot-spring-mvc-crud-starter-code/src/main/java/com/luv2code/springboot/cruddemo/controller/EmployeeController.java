cbec9589efe09fead593b6fe34ea7209201d4aeb
ahmed Elsherif <162807418+ahmed2005hussen@users.noreply.github.com>
2026-02-11 06:28:32 +0200

    final mvc

--- 06-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 06-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,8 +5,7 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -36,9 +35,45 @@ public class EmployeeController {
 
         model.addAttribute("employees", employees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 
+    @GetMapping("/showFormAdd")
+    public String showFormAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
 
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+
+        employeeService.save(employee);
+
+        // use a redirect to prevent duplicate submission
+        return "redirect:/employees/list";
+
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+
+        // get the employee from the service
+        Employee employee = employeeService.findById(id);
+
+        // set the employee in the model
+        model.addAttribute("employee", employee);
+
+        // send over to our form
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteEmployee(@RequestParam("employeeId") int id, Model model) {
+
+        employeeService.deleteById(id);
+        return "redirect:/employees/list";
+
+    }
 
 }
